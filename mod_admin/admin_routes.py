fcfecd376d0b73928a2da810dd085b32d084a8a0
Leonardo Salomon <leonardoperdona@hotmail.com>
2019-09-10 19:35:49 -0300

    Implemented list and forms products, requests, users

--- mod_admin/admin_routes.py
+++ mod_admin/admin_routes.py
@@ -3,8 +3,12 @@ from flask import Blueprint, render_template
 
 bp_admin = Blueprint('bp_admin', __name__, url_prefix='/admin', template_folder='templates', static_folder='static')
 
-@bp_admin.route('/pedidos') 
-def admin_requests():
+@bp_admin.route('/lista-pedidos') 
+def admin_list_requests():
+    return render_template('admin_list_requests.html')
+
+@bp_admin.route('/edit-pedidos') 
+def admin_edit_requests():
     return render_template('admin_requests.html')
 
 @bp_admin.route('/fornecedores') 
@@ -15,10 +19,18 @@ def admin_providers():
 def admin_your_requests():
     return render_template('admin_your_requests.html')
 
-@bp_admin.route('/produtos') 
-def admin_products():
+@bp_admin.route('/lista-produtos') 
+def admin_list_products():
+    return render_template('admin_list_products.html')
+
+@bp_admin.route('/edit-produtos') 
+def admin_edit_products():
     return render_template('admin_products.html')
 
-@bp_admin.route('/usuarios') 
-def admin_users():
+@bp_admin.route('/lista-usuarios') 
+def admin_list_users():
+    return render_template('admin_list_users.html')
+
+@bp_admin.route('/edit-usuarios') 
+def admin_edit_users():
     return render_template('admin_users.html')
